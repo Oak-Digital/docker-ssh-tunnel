d37eaca520ad84419482cb0df70c82d85c1245ef
Alexander Nortung <alexander.nortung@oakdigital.dk>
2024-08-15 13:16:56 +0200

    feat: allow using password

--- entry.sh
+++ entry.sh
@@ -1,2 +1,14 @@
 #! /usr/bin/env sh
-ssh -nNT -o ServerAliveInterval=$ALIVE_INTERVAL -o StrictHostKeyChecking=no -i $KEYFILE_LOCATION -L *:$LOCAL_PORT:0.0.0.0:${REMOTE_TARGET_PORT} $REMOTE_USER@$REMOTE_HOST -p $REMOTE_PORT
+HOST_STRING="$REMOTE_USER@$REMOTE_HOST"
+
+# If the password is defined add it to the host string
+if [ -n "$REMOTE_PASSWORD" ]; then
+    HOST_STRING="$REMOTE_USER:$REMOTE_PASSWORD@$REMOTE_HOST"
+fi
+
+ssh -nNT \
+    -o ServerAliveInterval=$ALIVE_INTERVAL \
+    -o StrictHostKeyChecking=no \
+    -i $KEYFILE_LOCATION \
+    -L *:$LOCAL_PORT:0.0.0.0:${REMOTE_TARGET_PORT} $HOST_STRING \
+    -p $REMOTE_PORT
